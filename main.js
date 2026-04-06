1e7c2e9459f690ae2715b6ff20dc1f8a49ea0bd2
Ari Chivukula <ari.c@icloud.com>
2026-04-06 10:30:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const dbx = new dropbox.Dropbox({
 });
 
 function getNYTC(date) {
+  console.log(`/svc/crosswords/v2/puzzle/print/${moment(date).format('MMMDDYY')}.pdf`);
   return new Promise((resolve, reject) => {
     const req = https.request({
       protocol: 'https:',
